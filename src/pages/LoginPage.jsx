02d12b08ac03500fd9861db49aff71eaa992f0ef
Vktr22 <nds.viktoria@gmail.com>
2026-03-30 10:05:27 +0200

    login form - email+jelszo+submit (onchange+alapertek)

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,6 +1,46 @@
 // src/pages/LoginPage.jsx
-import React from "react";
+import React, { useState } from "react";
 
 export default function LoginPage() {
-  return <h2>Login page</h2>;
+  // lokális state a form mezőkhöz
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // EGYELŐRE csak logolunk, NINCS backend hívás
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Login submit:", { email, password });
+  };
+
+  return (
+    <div className="login">
+      <h2>Bejelentkezés</h2>
+
+      <form onSubmit={handleSubmit}>
+        <div className="login-field">
+          <label htmlFor="email">Email cím</label>
+          <input
+            id="email"
+            type="email"
+            autoComplete="username"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)} //onchange--->mezobe iros esemeny kezelo
+          />
+        </div>
+
+        <div className="login-field">
+          <label htmlFor="password">Jelszó</label>
+          <input
+            id="password"
+            type="password"
+            autoComplete="current-password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+
+        <button type="submit">Belépés</button>
+      </form>
+    </div>
+  );
 }
