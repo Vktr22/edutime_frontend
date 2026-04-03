0a5b9df57cbcaf7507fe14ca017acc3da373294c
Vktr22 <nds.viktoria@gmail.com>
2026-04-03 17:24:55 +0200

    refactor(auth): use auth context in login page(redirect to home after successful login)

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,20 +1,24 @@
 // src/pages/LoginPage.jsx
 import React, { useState } from "react";
 import { useAuth } from "../contexts/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 export default function LoginPage() {
     const { login, serverError } = useAuth();
   // lokális state a form mezőkhöz
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const navigate = useNavigate(); //a komponens belsejeben minden renderkor lesz egy nav fgv amit barhol haszn(pl submit utan)
 
-  // EGYELŐRE csak logolunk, NINCS backend hívás
+  
   const handleSubmit = async(e) => {
+    e.preventDefault(); //SPA
     try {
         await login({ email, password });
-        console.log("Login succeeded, user loaded in AuthContext");
+        //tehat ha a login sikeres volt, mehetunk a /home-ra
+        navigate("/home");
     } catch (err) {
-        console.error("Login error in LoginPage:", err);
+        console.error("LoginPage handleSubmit error:", err);
     }
 
   };
