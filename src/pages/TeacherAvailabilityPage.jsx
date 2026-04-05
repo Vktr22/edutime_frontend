724de56af75cf0fd863dda8c8211b8a74468f38d
Vktr22 <nds.viktoria@gmail.com>
2026-04-05 16:48:23 +0200

    feat(availability): add state variables for list and errors

--- src/pages/TeacherAvailabilityPage.jsx
+++ src/pages/TeacherAvailabilityPage.jsx
@@ -2,17 +2,20 @@ import React from "react";
 import { useAuth } from "../contexts/AuthContext";
 
 export default function TeacherAvailabilityPage() {
-  const { user, loading } = useAuth();
+    const { user, loading } = useAuth();
+    //allapotok elokeszitese, h legyen hova tolteni az adatot----items, error
+    const [items, setItems] = useState([]);
+    const [error, setError] = useState("");
 
-  if (loading) return <p>Betöltés...</p>;
-  if (!user) return <p>Kérlek jelentkezz be.</p>;
-  if (user.role !== "teacher")
-    return <p>Ez az oldal csak tanároknak érhető el.</p>;
+    if (loading) return <p>Betöltés...</p>;
+    if (!user) return <p>Kérlek jelentkezz be.</p>;
+    if (user.role !== "teacher")
+        return <p>Ez az oldal csak tanároknak érhető el.</p>;
 
-  return (
-    <div>
-      <h2>Tanári elérhetőségek (munkaidősávok)</h2>
-      <p>Oldal betöltve. A funkciók következnek…</p>
-    </div>
-  );
+    return (
+        <div>
+        <h2>Tanári elérhetőségek (munkaidősávok)</h2>
+        <p>Oldal betöltve. A funkciók következnek…</p>
+        </div>
+    );
 }
