18e45732544b1b5c13cc7af1477075ae727cb258
Vktr22 <nds.viktoria@gmail.com>
2026-04-17 15:21:28 +0200

    feat(home): show cancellation notifications on

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
+import {
+  fetchStudentAppointments,
+  fetchTeacherAppointments,
+} from "../services/appointments";
 
 export default function HomePage() {
   const { user, loading } = useAuth();
+  const [notification, setNotification] = useState(null);
+
+  //ertesites home oldalon a masik felnek akit erint az idopont torles
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token || !user) return;
+
+    // DIÁK
+    if (user.role === "student") {
+      fetchStudentAppointments(token).then((data) => {
+        const hasCancelledByTeacher = data.some(
+          (appt) => appt.status === "cancelled_by_teacher",
+        );
+
+        if (hasCancelledByTeacher) {
+          setNotification("student_cancelled");
+        }
+      });
+    }
+
+    // TANÁR
+    if (user.role === "teacher") {
+      fetchTeacherAppointments(token).then((data) => {
+        const hasCancelledByStudent = data.some(
+          (appt) => appt.status === "cancelled_by_student",
+        );
+
+        if (hasCancelledByStudent) {
+          setNotification("teacher_cancelled");
+        }
+      });
+    }
+  }, [user]);
 
   if (loading) return <p>Loading user...</p>;
 
